43208c23627e0de35f2f0f69d14437c64287cf24
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-05-15 18:10:31 +0400

    added calculate/1 test cases

--- test/fibonacci_test.exs
+++ test/fibonacci_test.exs
@@ -5,4 +5,25 @@ defmodule FibonacciTest do
   test "greets the world" do
     assert Fibonacci.hello() == :world
   end
+
+  test "Fibnoacci.calculate/1 should give the correct result" do
+    assert Fibonacci.calculate(0) == {:ok, 0}
+    assert Fibonacci.calculate(2) == {:ok, 1}
+    assert Fibonacci.calculate(1) == {:ok, 1}
+    assert Fibonacci.calculate(3) == {:ok, 2}
+    assert Fibonacci.calculate(4) == {:ok, 3}
+    assert Fibonacci.calculate(5) == {:ok, 5}
+    assert Fibonacci.calculate(6) == {:ok, 8}
+    assert Fibonacci.calculate(7) == {:ok, 13}
+    assert Fibonacci.calculate(8) == {:ok, 21}
+    assert Fibonacci.calculate(100) == {:ok, 354_224_848_179_261_915_075}
+  end
+
+  test "Fibnoacci.calculate/1 with list element should give the correct result" do
+    assert Fibonacci.calculate([0, 1, 2]) == {:ok, [0, 1, 1]}
+    assert Fibonacci.calculate([0, 3, 5]) == {:ok, [0, 2, 5]}
+    assert Fibonacci.calculate([4, 6, 8]) == {:ok, [3, 8, 21]}
+    assert Fibonacci.calculate([11, 12, 16]) == {:ok, [89, 144, 987]}
+    assert Fibonacci.calculate([0, 1, 100]) == {:ok, [0, 1, 354_224_848_179_261_915_075]}
+  end
 end
